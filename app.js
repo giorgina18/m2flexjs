8c2e2e08d616fd03d0616470262217956bb468d1
Jouw Naam <studentnummer@ma-web.nl>
2023-12-01 12:50:30 +0100

    start_paars

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ const ingamestate_start = 0;
 const ingamestate_roll = 1;
 const ingamestate_end = 0;
 
+let gameState = game_start;
+let inGameState = ingamestate_start;
 let boardPositionSize = 50;
 let pawnPositions = [];
 let boardPositions = [];
 let playerAmountButtons = [];
+let uiwindow = createRect(600, 200, 300, 300)
+
 
 
 function createRect(x, y, w, h) {
@@ -32,19 +36,6 @@ function clearCanvas() {
     g.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function draw() {
-    clearCanvas();
-    for (let i = 0; i < boardPositions.length; i++) {
-        let pos = boardPositions[i];
-
-        g.fillStyle = "#004400";
-        //we gebruiken hier de x en y van het rectangle object
-        // vul bij de ??? ook de h & w in, net als bij de x en y gedaan is!
-        g.fillRect(pos.x, pos.y, pos.h, pos.w);
-        g.fillStyle = "#FFFFFF";
-        g.fillText((i + 1) + "", pos.x, pos.y + 20);
-    }
-}
 
 function createBoardPositions() {
     let x = 0;
@@ -72,5 +63,56 @@ function createBoardPositions() {
     }
 }
 
-createBoardPositions();
-draw();
+
+function initGame() {
+    createBoardPositions();
+
+    for (let i = 0; i < 4; i++) {
+        let button = createRect(uiwindow.x + 5 + (i * 50), uiwindow.y + 50, 50, 50);
+        button.playerAmount = i + 1;
+        playerAmountButtons.push(button)
+    }
+
+}
+
+
+
+function drawGameStart() {
+
+    for (let i = 0; i < playerAmountButtons.length; i++) {
+        // const element = playerAmountButtons[i];
+        g.fillStyle = "purple"
+        g.fillRect(playerAmountButtons[i].x, playerAmountButtons[i].y, playerAmountButtons[i].w, playerAmountButtons[i].h)
+
+        g.fillStyle = "#FFFFFF";
+        g.fillText((i + 1) + "", playerAmountButtons[i].x, playerAmountButtons[i].y + 20);
+
+    }
+}
+
+function drawIngame() {
+    for (let i = 0; i < boardPositions.length; i++) {
+        let pos = boardPositions[i];
+        g.fillStyle = "#004400";
+        g.fillRect(pos.x, pos.y, pos.w, pos.h);
+        g.fillStyle = "#FFFFFF";
+        g.fillText((i + 1) + "", pos.x, pos.y + 20);
+    }
+}
+function drawGameOver() { }
+
+function draw() {
+    clearCanvas();
+
+    if (gameState == game_start) { 
+        drawGameStart();
+    
+    }
+    
+    else if (gamestate == gamestate_ingame) {
+        drawIngame();
+    }
+}
+initGame()
+draw()
+
